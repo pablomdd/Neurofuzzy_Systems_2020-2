b640e66928682d595eb7d6dca1a403f127172816
pablomdd <pablo.mdd.007@gmail.com>
2020-06-07 19:53:05 -0500

    add competitive exercise

---
+++ 02-Neural_Networks/P12/index.m
@@ -0,0 +1,14 @@
+% P12: Competitive NN by Pablo Dominguez
+
+% Generate 5 sets with 10 random values each
+% • Multiply/divide/add/subtract each set by a different factor such that you separate
+% the sets slightly
+% • Plot all the points, each set in a different colour (please do not use yellow)
+% • Propose initial values for the weights as random (you need one weight per class)
+% • Plot the weights with a different symbol each
+% • Train the network to classify the 5 sets. (Ask the user for the number of epochs)
+% • Test the network for all possible values in the cartesian plane such that you
+% include the whole range of your random separated sets. Plot each class in a
+% different colour
+% • Upload your report including the code and the graphs, before and after the
+% trainning
---
+++ 02-Neural_Networks/competitive_nn.m
@@ -0,0 +1,64 @@
+% Competitive Neural Networks Exercise
+clc, clear all, close all
+p1 = [-0.1961; 0.9806];
+p2 = [0.1961; 0.9806];
+p3 = [0.9806; 0.1961];
+p4 = [0.9806; -0.1961;];
+p5 = [-0.5812; -0.8137];
+p6 = [-0.8137; -0.5812];
+
+w1 = [0.7071; -0.7071];
+w2 = [0.7071; 0.7071];
+w3 = [-1.0000; 0.0000];
+
+P = [p1 p2 p3 p4 p5 p6];
+W = [w1'; w2'; w3'];
+[m, n] = size(P);
+
+
+%% Training
+alpha = 0.2;
+epochs = 20;
+D = zeros(3,2);
+L = zeros(3,1);
+
+for i = 1 : epochs
+    for k = 1: length(W)
+        plot(W(k,1), W(k,2), 'pm', 'LineWidth',2)
+        hold on
+    end
+    for j = 1 : n       
+        a = compet(W * P(:,j));
+        
+        W = W + alpha * a .* (P(:,j)' - W);
+    end
+end
+%% Plotting
+figure(1)
+s = 'Competitive Neural Networks';
+title(s);
+hold on
+for i = 1 : n/3
+    plot(P(1,i), P(2,i), '*b')
+    hold on
+end
+for i = (n/3 + 1): (2*n/3)
+    plot(P(1,i), P(2,i), '*b')
+    hold on
+end
+for i = (2*n/3 + 1): (n)
+    plot(P(1,i), P(2,i), '*b')
+    hold on
+end
+
+% Plot W
+for i = 1: length(W)
+    plot(W(i,1), W(i,2), 'pg', 'LineWidth',2)
+    hold on
+end
+
+grid on
+xlim([-2 2])
+ylim([-1 1])
+    
+    
